02775946ec91448fa3c8afdc75f4c3cd571ee027
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-02-01 21:05:21 -0600

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+
+variable "public_subnets" {
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  description = "CIDR blocks for public subnets"
+}
+variable "private_subnets" {
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "CIDR blocks for private subnets"
+}
