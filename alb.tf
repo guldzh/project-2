241163564ad61cbbf92f5ffccd8069a00d980bfc
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-02-02 17:21:37 -0600

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_lb" "my_alb" {
   name               = "my-alb"
   internal           = false
@@ -33,6 +34,7 @@ resource "aws_lb_listener" "my_listener" {
     target_group_arn = aws_lb_target_group.my_target_group.arn # Specify the ARN of your target group
   }
 }
+*/
 
 
 
