578d94917128f57d5ced28d16f709c33939fd5e0
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-02-01 21:05:03 -0600

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,91 @@
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "project-vpc"
+  }
+}
+#Internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "project-igw"
+  }
+}
+#Public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = 3
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+#Public Subnets
+resource "aws_subnet" "public_subnet" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+#Private Route table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+#Private subnets
+
+resource "aws_subnet" "private_subnet" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = false
+
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = 3
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_eip" "nat_gateway_eip" {
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+  tags = {
+    Name = "project-nat"
+  }
+}
+resource "aws_route" "private_subnet_route_to_nat" {
+  route_table_id         = aws_route_table.private_route_table.id
+  nat_gateway_id         = aws_nat_gateway.nat.id
+  destination_cidr_block = "0.0.0.0/0"
+}
