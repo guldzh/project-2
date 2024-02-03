a256ac90ddfb5194e660b10fd24e9a4067e14085
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-02-02 19:18:54 -0600

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,23 +2,55 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
+
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
+
 variable "availability_zones" {
   type    = list(string)
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
-
 
 variable "public_subnets" {
   type        = list(string)
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   description = "CIDR blocks for public subnets"
 }
+
 variable "private_subnets" {
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   description = "CIDR blocks for private subnets"
+}
+
+variable "cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+  default     = "my-ecs-cluster"
+}
+
+variable "task_family" {
+  description = "Family name of the ECS task definition"
+  type        = string
+  default     = "myapp-task"
+}
+
+variable "container_image" {
+  description = "Docker image for the container"
+  type        = string
+  default     = "islamovna/my-nginx-image:latest"
+}
+
+variable "container_cpu" {
+  description = "CPU units for the container"
+  type        = number
+  default     = "256"
+}
+
+variable "container_memory" {
+  description = "Memory for the container"
+  type        = number
+  default     = "512"
 }
