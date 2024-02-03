7515bccc4aafd71420e9f87975be7e394fe6d6b2
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-02-02 19:19:47 -0600

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,24 +1,24 @@
 # Define ECS Cluster
 resource "aws_ecs_cluster" "main" {
-  name = "my-ecs-cluster"
+  name = var.cluster_name
 }
 
 # Define ECS Task Definition
 resource "aws_ecs_task_definition" "app" {
-  family                   = "myapp-task"
+  family                   = var.task_family
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = var.container_cpu
+  memory                   = var.container_memory
 
 
   container_definitions = jsonencode([
     {
       name      = "my-app"
-      image     = "islamovna/my-nginx-image:latest" # Replace with your Docker image
-      cpu       = 256
-      memory    = 512
+      image     = var.container_image # Replace with your Docker image
+      cpu       = var.container_cpu
+      memory    = var.container_memory
       essential = true
 
       portMappings : [
@@ -48,7 +48,7 @@ resource "aws_ecs_service" "my_service" {
 
   network_configuration {
     subnets          = [aws_subnet.private_subnet[0].id] # Specify your public subnets
-    security_groups  = [aws_security_group.ecs_sg.id]   # Specify the security group you created
+    security_groups  = [aws_security_group.ecs_sg.id]    # Specify the security group you created
     assign_public_ip = true
   }
   depends_on = [aws_lb_listener.my_listener, aws_iam_role_policy_attachment.ecs_task_execution_role]
