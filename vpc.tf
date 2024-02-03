47dff42a40b9e8f892064e88b871b5f9c4c8d58b
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-02 20:44:52 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -78,7 +78,8 @@ resource "aws_eip" "nat_gateway_eip" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  subnet_id     = aws_subnet.public_subnet[0].id 
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
   tags = {
     Name = "project-nat"
   }
