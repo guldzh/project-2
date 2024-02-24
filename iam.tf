0c9014a56d077f296228113f75ad553c3497e8d5
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-24 11:44:40 -0600

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,23 +1,22 @@
-data "aws_iam_policy_document" "ecs_task_execution_role" {
-  version = "2012-10-17"
+data "aws_iam_policy_document" "assume_role" {
   statement {
-    sid     = ""
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
+    effect = "Allow"
 
     principals {
       type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
+      identifiers = ["eks.amazonaws.com"]
     }
+
+    actions = ["sts:AssumeRole"]
   }
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+resource "aws_iam_role" "example" {
+  name               = "eks-cluster-example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.example.name
 }
