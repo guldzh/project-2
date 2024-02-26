86f1807043ec2fc595f7e9638f7e8da7dab35907
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-26 14:23:42 -0600

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,37 +1,52 @@
-resource "aws_eks_cluster" "eks-cluster" {
-  name     = "eks-cluster"
+resource "aws_eks_cluster" "eks" {
+  # Name of the cluster.
+  name = "eks"
+
+  # The Amazon Resource Name (ARN) of the IAM role that provides permissions for 
+  # the Kubernetes control plane to make calls to AWS API operations on your behalf
   role_arn = aws_iam_role.example.arn
-  node_group_name      = "workers"
-  node_group_desired_capacity = 1
-  node_group_instance_type   = "t3.medium"
+
+  # Desired Kubernetes master version
+  version = "1.27"
 
   vpc_config {
-    subnet_ids = [aws_subnet.private_subnets1.id, aws_subnet.private_subnets2.id,aws_subnet.private_subnets3.id]
+    # Indicates whether or not the Amazon EKS private API server endpoint is enabled
+    endpoint_private_access = false
+
+    # Indicates whether or not the Amazon EKS public API server endpoint is enabled
+    endpoint_public_access = true
+
+    # Must be in at least two different availability zones
+    subnet_ids = [
+      aws_subnet.public_subnets1.id,
+      aws_subnet.public_subnets2.id,
+      aws_subnet.private_subnets1.id,
+      aws_subnet.private_subnets2.id
+    ]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy
   ]
 }
 
 resource "aws_eks_addon" "cni" {
-  cluster_name = aws_eks_cluster.eks-cluster.name
+  cluster_name = aws_eks_cluster.eks.name
   addon_name   = "vpc-cni"
 }
 
 resource "aws_eks_addon" "csi" {
-  cluster_name = aws_eks_cluster.eks-cluster.name
+  cluster_name = aws_eks_cluster.eks.name
   addon_name   = "aws-ebs-csi-driver"
 }
 
 output "endpoint" {
-  value = aws_eks_cluster.eks-cluster.endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.eks-cluster.certificate_authority[0].data
+  value = aws_eks_cluster.eks.certificate_authority[0].data
 }
 
