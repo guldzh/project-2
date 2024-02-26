5745e0ca375424ae64411228f343b9140f96fa3b
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-26 14:44:26 -0600

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -2,16 +2,14 @@ resource "aws_security_group" "sg" {
   name   = "sg"
   vpc_id = aws_vpc.main.id
 
-  ingress = 
-    {
+  ingress {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
 
-  egress = 
-    {
+  egress {
       from_port   = 0
       to_port     = 65535
       protocol    = "tcp"
