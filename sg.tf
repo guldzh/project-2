e10f84a52cd6d43b88bf16c36717bb42604ba7fd
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-26 19:46:13 -0600

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -17,4 +17,34 @@ resource "aws_security_group" "sg" {
     }
 }
 
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for Application Load Balancer"
+
+  vpc_id = aws_vpc.main.id # Specify your VPC ID
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from anywhere
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow outbound traffic to anywhere
+  }
+  tags = {
+    Name = "eks-alb-sg"
+  }
+}
 
