2d5f96963232216c5562e25bfa816172073e8b34
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-26 20:09:52 -0600

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,35 +1,13 @@
-resource "aws_lb" "my_alb" {
-  name               = "my-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]                          # Associate the ALB with the security group
-  subnets            = [for subnet in aws_subnet.public_subnet[*] : subnet.id] # Specify your public subnets
-}
+module "lb_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-resource "aws_lb_target_group" "my_target_group" {
-  name        = "my-target-group"
-  port        = 80              # Port that your ECS tasks or containers are listening on
-  protocol    = "HTTP"          # Protocol used by your ECS tasks or containers
-  vpc_id      = aws_vpc.main.id # Specify your VPC ID
-  target_type = "ip"
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    interval            = 30
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200"
-  }
-}
+  role_name = "eks_lb"
+  attach_load_balancer_controller_policy = true
 
-resource "aws_lb_listener" "my_listener" {
-  load_balancer_arn = aws_lb.my_alb.arn # Specify the ARN of your ALB
-  port              = 80                # Port on which the ALB will listen for incoming traffic
-  protocol          = "HTTP"            # Protocol used by the ALB
-
-  default_action {
-    type             = "forward"                               # Action to take for incoming requests
-    target_group_arn = aws_lb_target_group.my_target_group.arn # Specify the ARN of your target group
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
   }
 }
