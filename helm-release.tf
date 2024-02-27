d0bed6fbee965e074f8b2649962cb0cde65c1f8e
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-26 20:16:58 -0600

    Create helm-release.tf

---
+++ helm-release.tf
@@ -0,0 +1,34 @@
+resource "helm_release" "lb" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  depends_on = [
+    kubernetes_service_account.service-account
+  ]
+
+  set {
+    name  = "region"
+    value = "us-east-1"
+  }
+
+  set {
+    name  = "vpcId"
+    value = aws_vpc.main.id
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks.name
+  }
+}
