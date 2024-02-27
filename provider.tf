f4e065d87e80ba642cfb0f963f5f53c579e2b10d
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-26 20:24:27 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = aws_eks_cluster_auth.eks.token
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
@@ -26,7 +26,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
       args        = ["eks", "get-token", "--cluster-name", "eks"]
