06a61b2f46acef9048cc843ea8b070e9ac69a765
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-27 19:34:59 -0600

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -20,26 +20,3 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.example.name
 }
-
-resource "kubernetes_cluster_role" "admin_role" {
-  metadata {
-    name = "k8s-admin-role"
-    labels = {
-      name = "AdminRole"
-    }
-  }
-
-  rule {
-    api_groups = ["", "apps", "batch", "extensions"]
-    resources = [
-      "pods", "cronjobs", "deployments", "devents", "ingresses",
-      "jobs", "pods/attach", "pods/exec", "pods/log", "pods/portfoward",
-      "secrets", "services", "nodes"
-    ]
-    verbs = [
-      "get", "list", "watch", "delete", "describe", "patch",
-    "update", "create"]
-  }
-
-  depends_on = [aws_eks_node_group.nodes_general]
-}
