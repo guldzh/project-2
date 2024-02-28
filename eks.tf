1d461bc2e9aa32ff3eb2acec59590473895e8962
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-27 18:11:13 -0600

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -50,10 +50,10 @@ output "endpoint" {
   value = aws_eks_cluster.eks.endpoint
 }
 
-output "kubeconfig" {
-  value = module.eks-kubeconfig.kubeconfig
-  sensitive = true
-}
+#output "kubeconfig" {
+#  value = module.eks-kubeconfig.kubeconfig
+ # sensitive = true
+#}
 
 #output "kubeconfig-certificate-authority-data" {
 #  value = aws_eks_cluster.eks.certificate_authority[0].data
