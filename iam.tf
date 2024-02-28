8eb8fca1fc5b2b6cda079110e014d97c34d53219
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-27 20:19:22 -0600

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -13,6 +13,11 @@ data "aws_iam_policy_document" "assume_role" {
 statement {
     effect = "Allow"
 
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
     actions = [
       "iam:CreateServiceAccount",
       "iam:DeleteServiceAccount",
