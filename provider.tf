952560b41df67cdc333a5555cdc3c349f2cb8f2b
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-27 20:07:43 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -35,7 +35,7 @@ provider "helm" {
   }
 }
 
-/*resource "kubernetes_service_account" "service-account" {
+resource "kubernetes_service_account" "service-account" {
   metadata {
     name = "aws-load-balancer-controller"
     namespace = "kube-system"
@@ -43,11 +43,7 @@ provider "helm" {
         "app.kubernetes.io/name"= "aws-load-balancer-controller"
         "app.kubernetes.io/component"= "controller"
     }
-    annotations = {
-      "eks.amazonaws.com/role-arn" = module.lb_role.iam_role_arn
-      "eks.amazonaws.com/sts-regional-endpoints" = "true"
-    }
   }
 }
-*/
+
 
