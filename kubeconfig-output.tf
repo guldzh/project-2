410e1a0fd7567aac6345aae6650ad0be09ad7044
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-28 14:04:29 -0600

    Update kubeconfig-output.tf

--- kubeconfig-output.tf
+++ kubeconfig-output.tf
@@ -3,7 +3,7 @@ module "eks-kubeconfig" {
   version = "2.0.0"
   cluster_name = "eks"
   depends_on = [
-    aws_eks_cluster.eks                                                                                                                            
+    aws_eks_cluster.eks,                                                                                                                            
     aws_eks_node_group.nodes_general
   ]
 }
