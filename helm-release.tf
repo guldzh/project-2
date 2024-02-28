d926221ece26ba6da37bf3912e7643bf8194e130
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-28 15:22:18 -0600

    Update helm-release.tf

--- helm-release.tf
+++ helm-release.tf
@@ -1,11 +1,11 @@
-resource "helm_release" "lb" {
+/*resource "helm_release" "lb" {
   name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   version    = "1.7.1"
   namespace  = "kube-system"
   depends_on = [
-    #kubernetes_service_account.service-account,
+    kubernetes_service_account.service-account,
     aws_eks_cluster.eks,                                                                                                                            
     aws_eks_node_group.nodes_general
   ]
@@ -34,4 +34,4 @@ resource "helm_release" "lb" {
     name  = "clusterName"
     value = aws_eks_cluster.eks.name
   }
-}
+}*/
